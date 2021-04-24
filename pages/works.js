d58502ba047a1a2fbfb75725c46851fb5859ff25
okumujustine <okumujustine01@gmail.com>
2021-04-24 09:27:00 +0300

    my personal website setup

---
+++ pages/works.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Container from "../components/Container";
+
+const works = () => {
+  return (
+    <Container>
+      <p>My works</p>
+    </Container>
+  );
+};
+
+export default works;
