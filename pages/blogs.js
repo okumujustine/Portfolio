12c29b967232df9ac58a97c472b0f4df401ed63f
okumujustine <okumujustine01@gmail.com>
2021-05-21 03:11:28 +0300

    added more sections

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Head from "next/head";
 
-const Blogs = () => {
+
+export default function Blogs() {
   return (
     <div>
-            <Head>
+      <Head>
         <title>Blog | Okumu Justine</title>
       </Head>
       <p>My blogs</p>
     </div>
   );
 };
-
-export default Blogs;
