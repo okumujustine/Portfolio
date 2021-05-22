38ec17f4e2e2422470e96a9e361bc6cc72df0a84
okumujustine <okumujustine01@gmail.com>
2021-05-22 03:03:26 +0300

    project loader and data dirs/files

---
+++ pages/data/projects/list.js
@@ -0,0 +1,6 @@
+
+
+export const projects = [
+      {link: "https://jobsug.netlify.app/", projectTitle:"JOB PORTAL",  myTitle:"Developer/Creator", videoLink:"https://www.youtube.com/watch?v=Q-U3voOZukg" ,description: "A job application website for managing job applications by both employees and employers."},
+      {link: "https://www.mumble.dev/", projectTitle:"MUMBLE", myTitle:"Opensource contributor", videoLink:"https://www.youtube.com/watch?v=2c8SIX0VsSc&t=474s" ,description: "Mumble is an opensource social media platform and public forum for questions and discussions."}
+    ]
--- pages/projects.js
+++ pages/projects.js
@@ -1,24 +1,31 @@
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Head from "next/head";
 
 import Card from "../components/card"
+import CardLoader from "../components/card-loader"
+import {projects} from "./data/projects/list"
 
 const Projects = () => {
-  const projects = [
-    {link: "https://jobsug.netlify.app/", projectTitle:"JOB PORTAL",  myTitle:"Developer/Creator", videoLink:"https://www.youtube.com/watch?v=Q-U3voOZukg" ,description: "A job application website for managing job applications by both employees and employers."},
-    {link: "https://www.mumble.dev/", projectTitle:"MUMBLE", myTitle:"Opensource contributor", videoLink:"https://www.youtube.com/watch?v=2c8SIX0VsSc&t=474s" ,description: "Mumble is an opensource social media platform and public forum for questions and discussions."}
-  ]
+  const [localProjects, setLocalProjects] = useState([])
+
+  useEffect(() =>{
+    setTimeout(() => {
+      setLocalProjects(projects)
+    }, 1500)
+  }, [])
+
   return (
     <div>
         <Head>
         <title>Projects | Okumu Justine</title>
       </Head>
       <h2 className="text-2xl text-gray-800 font-bold">My Projects</h2>
+      <>{localProjects.length === 0 && <div className="flex justify-center items-center lg:mt-40"><CardLoader/></div>}</>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4"> 
-     {projects.length > 0 && projects.map((project, index) => {
+     {localProjects.length > 0 && projects.map((project, index) => {
      return <Card key={index} projectTitle={project.projectTitle} link={project.link} videoLink={project.videoLink} description={project.description} myTitle={project.myTitle}  />
-    }) }
+      }) }
        </div>
     </div>
   );
