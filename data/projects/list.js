d960891e806b9addedffc2014564b282771e3864
okumujustine <okumujustine01@gmail.com>
2021-05-31 14:42:27 +0300

    added project to portfolio

--- data/projects/list.js
+++ data/projects/list.js
@@ -1,6 +1,7 @@
 
 
 export const projects = [
-      {link: "https://jobsug.netlify.app/", projectTitle:"JOB PORTAL",  myTitle:"Developer/Creator", videoLink:"https://www.youtube.com/watch?v=Q-U3voOZukg" ,description: "A job application website for managing job applications by both employees and employers."},
-      {link: "https://www.mumble.dev/", projectTitle:"MUMBLE", myTitle:"Opensource contributor", videoLink:"https://www.youtube.com/watch?v=2c8SIX0VsSc&t=474s" ,description: "Mumble is an opensource social media platform and public forum for questions and discussions."}
-    ]
+  { link: "https://jobsug.netlify.app/", imageLink: "", projectTitle: "JOB PORTAL", myTitle: "Developer/Creator", videoLink: "https://www.youtube.com/watch?v=Q-U3voOZukg", description: "A job application website for managing job applications by both employees and employers." },
+  { link: "https://www.mumble.dev/", imageLink: "", projectTitle: "MUMBLE", myTitle: "Opensource contributor", videoLink: "https://www.youtube.com/watch?v=2c8SIX0VsSc&t=474s", description: "Mumble is an opensource social media platform and public forum for questions and discussions." },
+  { link: "https://flt-invoice.netlify.app/", imageLink: "dist/images/invoiceapp.png", projectTitle: "INVOICE UI, MOCKAPI", myTitle: "Simple project", videoLink: "", description: "Invoice UI application for creating customers, products and giving invoices consuming MOCKAPI" },
+]
--- pages/projects.js
+++ pages/projects.js
@@ -1,15 +1,15 @@
 
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 
 import Card from "../components/card"
 import CardLoader from "../components/card-loader"
-import {projects} from "../data/projects/list"
+import { projects } from "../data/projects/list"
 
 const Projects = () => {
   const [localProjects, setLocalProjects] = useState([])
 
-  useEffect(() =>{
+  useEffect(() => {
     setTimeout(() => {
       setLocalProjects(projects)
     }, 1500)
@@ -17,16 +17,16 @@ const Projects = () => {
 
   return (
     <div>
-        <Head>
+      <Head>
         <title>Projects | Okumu Justine</title>
       </Head>
       <h2 className="text-2xl text-gray-800 font-bold pb-4">My Projects and Contributions.</h2>
-      <>{localProjects.length === 0 && <div className="flex justify-center items-center lg:mt-40"><CardLoader/></div>}</>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4"> 
-     {localProjects.length > 0 && projects.map((project, index) => {
-     return <Card key={index} projectTitle={project.projectTitle} link={project.link} videoLink={project.videoLink} description={project.description} myTitle={project.myTitle}  />
-      }) }
-       </div>
+      <>{localProjects.length === 0 && <div className="flex justify-center items-center lg:mt-40"><CardLoader /></div>}</>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        {localProjects.length > 0 && projects.map((project, index) => {
+          return <Card key={index} {...project} />
+        })}
+      </div>
     </div>
   );
 };
