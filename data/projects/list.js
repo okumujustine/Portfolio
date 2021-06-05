d7ae6fb4add4098a1a01243afc814a25c29fd785
okumujustine <okumujustine01@gmail.com>
2021-06-05 22:29:48 +0300

    added a new project

--- data/projects/list.js
+++ data/projects/list.js
@@ -2,6 +2,7 @@
 
 export const projects = [
   { link: "https://jobsug.netlify.app/", imageLink: "", projectTitle: "JOB PORTAL", myTitle: "Developer/Creator", videoLink: "https://www.youtube.com/watch?v=Q-U3voOZukg", description: "A job application website for managing job applications by both employees and employers." },
+  { link: "https://collabediting.netlify.app/", imageLink: "", projectTitle: "COLLAB EDITING", myTitle: "Developer", videoLink: "https://www.youtube.com/watch?v=Gj6RpWbcAsc", description: "Create a note/document and invite friends through their email address to collaboratively edit with you in realtime." },
   { link: "https://www.mumble.dev/", imageLink: "", projectTitle: "MUMBLE", myTitle: "Opensource contributor", videoLink: "https://www.youtube.com/watch?v=2c8SIX0VsSc&t=474s", description: "Mumble is an opensource social media platform and public forum for questions and discussions." },
   { link: "https://flt-invoice.netlify.app/", imageLink: "dist/images/invoiceapp.png", projectTitle: "INVOICE UI, MOCKAPI", myTitle: "Simple project", videoLink: "", description: "Invoice UI application for creating customers, products and giving invoices consuming MOCKAPI" },
 ]
