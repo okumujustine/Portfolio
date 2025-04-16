80df1b8759b4ba7c0ea660825246dc1a32073d3e
Justine Okumu <okumujustine01@gmail.com>
2025-04-15 22:04:58 -0500

    refactor: update project structure and dependencies
    - Renamed project from "personal-website" to "pw" in package.json.
    - Removed unused dependencies and updated React and Next.js to latest versions.
    - Deleted several unused pages and components including _app.js, _documents.js, blogs.js, and others.
    - Removed custom CSS files and replaced with Tailwind CSS configuration.
    - Added TypeScript support with a new tsconfig.json file.
    - Updated PostCSS configuration to use ES module syntax.
    - Removed various binary files and images that are no longer needed.

--- components/crubs.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Crubs({ text }) {
-    return (
-        <div className="text-1xl py-3 px-4 mt-2 text-center rounded-sm ml-1 bg-gray-800 text-white">{text}</div>
-    )
-}
--- components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <div className=" h-14 lg:h-16 bg-gray-900 mt-6 lg:mt-8 flex justify-center items-center">
-            <span className="text-white font-bold">Okumu Justine {`@ ${new Date().getFullYear()}`}</span>
-        </div>
-    )
-}
